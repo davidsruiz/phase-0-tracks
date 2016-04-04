04e9987a98670227dd24b35c4f7aa35232aee13c
David Ruiz <druiz2412@gmail.com>
2016-04-04 11:51:49 -0400

    updated method detective file

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+# => "InVeStIgAtIoN"
 
-# “zom”.<???>
-# => “zoom”
+"zom".insert(2, "o")
+# => "zoom"
 
-# “enhance”.<???>
-#=> “    enhance    ”
+"enhance".center(15)
+#=> "    enhance    "
 
-# “Stop! You’re under arrest!”.<???>
-# => “STOP! YOU’RE UNDER ARREST!”
+"Stop! You’re under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!"
 
-# “the usual”.<???>
-#=> “the usual suspects”
+"the usual".concat(" suspects")
+#=> "the usual suspects"
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+" suspects".prepend("the usual")
+#=> "the usual suspects"
 
-# “The case of the disappearing last letter”.<???>
-# => “The case of the disappearing last lette”
+"The case of the disappearing last letter".slice!(0..-2)
+# => "The case of the disappearing last lette"
 
-# “The mystery of the missing first letter”.<???>
-# => “he mystery of the missing first letter”
+"The mystery of the missing first letter".slice!(1..-1)
+# => "he mystery of the missing first letter"
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
-# => “Elementary, my dear Watson!”
+"Elementary  ,    my   dear      Watson!".gsub(/\s+/, '')
+# => "Elementary, my dear Watson!"
 
-#“z”.<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# Answer: Returns the corresponding ASCII value for the character
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+"How many times does the letter ‘a’ appear in this string?".scan("a").length
 #=> 4
