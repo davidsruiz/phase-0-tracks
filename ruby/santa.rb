7947281865c51b9f2ee347bdc3d4f508c7695b10
David Ruiz <druiz2412@gmail.com>
2016-04-11 11:11:11 -0400

    finished 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,11 @@
 
 class Santa
+	attr_reader :gender, :ethnicity, :reindeer_ranking
+	attr_accessor :age
+	#attr_writer - creates writable
 
 	def initialize(gender, ethnicity)
-		puts "Initializing Santa instance ..."
+		# puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
@@ -32,14 +35,77 @@ class Santa
   end
 
   def get_mad_at(reindeer)
-  	if 
+  	@reindeer_ranking << @reindeer_ranking.delete(reindeer)
   end
 
 end
 
 
+=begin
+====  DRIVER CODE ====
 Joe = Santa.new("modern", "baconColored")
 Joe.speak
 Joe.eat_milk_and_cookies("snickerdoodle")
-
+Joe.get_mad_at("Dasher")
 p Joe.reindeer_ranking
+
+p Joe.gender
+
+=end
+
+
+
+def return_random(arr)
+	return arr[rand(arr.length)]
+end
+
+def create_random_santa
+	genders = [
+	"Agender",
+	"Androgyne/Androgynous",
+	"Bigender",
+	"Female to Male/ FTM",
+	"Gender Fluid",
+	"Gender Nonconforming",
+	"Gender Questioning",
+	"Gender Variant",
+	"Genderqueer",
+	"Intersex",
+	"Male to Female/MTF"
+]
+
+ethnicities = [
+	"Zhuang",
+	"Volga Tatars",
+	"Uyghur",
+	"Turkmens",
+	"Tuaregs",
+	"Thais",
+	"Tamils",
+	"Sundanese",
+	"Slovenes",
+	"Sinhalese"
+]
+	santa = Santa.new(return_random(genders), return_random(ethnicities))
+	santa.age = rand(140)
+	return santa
+end
+
+def print_santa(santa)
+	print santa.gender
+	print " "
+	print santa.ethnicity
+	print " "
+	puts santa.age
+end
+
+200.times { |i| 
+	print "Santa #{i+1} - "
+	print_santa create_random_santa
+}
+
+
+
+
+
+
