739fb552e907b451e2bd335d03c5aedf9961ce00
David Ruiz <druiz2412@gmail.com>
2016-04-12 09:43:08 -0400

    added meta info at top

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,8 +1,8 @@
 
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Gregor Destrée].
+# We spent [1:20] hours on this challenge.
 
 # EXPLANATION OF require_relative
 # require_relative has a local data load path as opposed to require which can be
