3476328297e4e37854e8ccf952f16bbac373e175
David Ruiz <druiz2412@gmail.com>
2016-04-18 10:43:35 -0400

    updated data structures part 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -9,3 +9,12 @@ colors.push("purple");
 names.push("Ed");
 
 console.log(colors, names);
+
+//assign colors to horses
+
+var horses = {};
+
+for (var i = 0; i < names.length; i++) {
+  horses[names[i]] = colors[i];
+}
+console.log(horses);
