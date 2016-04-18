24f715ba57ea68c369e1afa411d2bd8556e9a813
David Ruiz <druiz2412@gmail.com>
2016-04-18 11:21:49 -0400

    updated algos.js part 1

--- js/algos.js
+++ js/algos.js
@@ -12,6 +12,26 @@ function longestStrIn(arr) {
 	}
 }
 
+
+function findKeyValueMatch(obj1, obj2) {
+	var matchFound = false;
+	for(var key1 in obj1){
+    for(var key2 in obj2){
+	    if(key1 == key2 && obj1[key1] == obj2[key2]) matchFound = true;
+		}
+	}
+	return matchFound;
+}
+
+
 log(longestStrIn(["a", "ab", "abcc", "abd"]));
 log(longestStrIn(["123", "234", "345", "456"]));
 log(longestStrIn(["1900", "a", "b", "1600"]));
+
+var obj1 = {cow: "moo", alive: true}
+var obj2 = {cow: "moo", alive: false}
+var obj3 = {cow: "bleh", alive: true}
+var obj4 = {cow: "bleh", alive: false}
+log(findKeyValueMatch(obj1, obj2));
+log(findKeyValueMatch(obj1, obj3));
+log(findKeyValueMatch(obj1, obj4));
