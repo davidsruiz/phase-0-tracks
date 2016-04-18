c3f81b863a1b77c57c2f01cf1820a25ba4e562aa
David Ruiz <druiz2412@gmail.com>
2016-04-18 10:44:04 -0400

    updated data structures part 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -18,3 +18,24 @@ for (var i = 0; i < names.length; i++) {
   horses[names[i]] = colors[i];
 }
 console.log(horses);
+
+function Car(color, year, isManual) {
+  this.color = color;
+  this.year = year;
+  this.isManual = isManual;
+  
+  this.drive = function() {
+    console.log("Vroom!");
+  };
+}
+
+// create a few different cars 
+
+var newCar = new Car("blue", 2010, false);
+var oldCar = new Car("green", 1982, true);
+var fastCar = new Car("red", 2017, true);
+
+newCar.drive();
+oldCar.drive();
+
+console.log(newCar, oldCar, fastCar);
