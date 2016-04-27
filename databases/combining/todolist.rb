77d9ba83244636932fdcd0efbcbd13349553b38a
David Ruiz <druiz2412@gmail.com>
2016-04-27 03:38:09 -0400

    list project continuation

--- databases/combining/todolist.rb
+++ databases/combining/todolist.rb
@@ -6,7 +6,8 @@ $db = SQLite3::Database.new("list.db")
 create_table_cmd = <<-SQL
 	CREATE TABLE IF NOT EXISTS list (
 		id INTEGER PRIMARY KEY,
-		entry VARCHAR(255)
+		entry VARCHAR(255),
+		done BOOLEAN
 	)
 SQL
 
@@ -15,24 +16,61 @@ $db.execute(create_table_cmd);
 list_name = "list"
 
 
-def print_list(list)
-	puts list_count_string(list)
 
-	arr = $db.execute("SELECT * from #{list}")
+def print_list(list, modifier="all")
+	query = "SELECT * from #{list}"
+
+	case modifier
+	when "all"
+		query = query + ";"
+	when "done"
+		query = query + " WHERE done='true';"
+	when "todo"
+		query = query + " WHERE done='false';"
+	else
+		err "no view of type #{modifier}"
+		return
+	end
+
+	arr = $db.execute(query)
+
+	puts "#{arr.length} " + plural("Item", arr.length)
+
 	arr.each do |row|
-		puts "- #{row[1]}"
+		puts "[" + (row[2] == "true" ? "+" : " ") + "] #{row[1]}"
 	end
 end
 
 def process_addition(entry, list)
 
-	add_to_list(entry, list)
-	puts "'#{entry}' was added. list count: #{list_count(list)}"
+	if entry != ""
+		add_to_list(entry, list)
+		puts "'#{entry}' was added. list count: #{list_count(list)}"
+	else
+		err "not a valid entry!"
+	end
 
 end
 
 def add_to_list(entry, list)
-	$db.execute("INSERT INTO #{list} (entry) VALUES ('#{entry}')")
+	$db.execute("INSERT INTO #{list} (entry, done) VALUES ('#{entry}', 'false')")
+end
+
+def process_completion(entry, list)
+
+	result = $db.execute("SELECT * from #{list} WHERE entry='#{entry}';")
+
+	if result.length > 0
+		complete_in_list(entry, list)
+		puts "'#{entry}' is now checked off!"
+	else
+		err "'#{entry}' not found!"
+	end
+
+end
+
+def complete_in_list(entry, list)
+	$db.execute("UPDATE #{list} SET done='true' WHERE entry='#{entry}';")
 end
 
 def process_removal(entry, list)
@@ -56,7 +94,7 @@ end
 def process_clearance(list)
 
 	clear_list(list)
-	puts "list '#{list}' was cleared. list count: #{list_count(list)}"
+	puts "'#{list}' was cleared. list count: #{list_count(list)}"
 
 end
 
@@ -64,33 +102,64 @@ def clear_list(list)
 	$db.execute("DELETE FROM #{list};")
 end
 
+def process_clean(list)
+
+	old_count = list_count(list)
+	clean_list(list)
+	new_count = list_count(list)
+	diff = old_count - new_count
+
+	puts "clean finished. #{diff} " + plural("item", diff) + " removed"
+
+end
+
+def clean_list(list)
+
+	$db.execute("DELETE FROM #{list} WHERE done='true';")
+
+end
+
 def list_count_string(list)
 	count = list_count(list)
-	"#{count} Item" + ( count == 1 ? "" : "s" )
+	"#{count} " + plural("Item", count)
 end
 
 def list_count(list)
 	$db.execute("SELECT COUNT(*) FROM #{list}")[0][0]
 end
 
+def plural(word, amount)
+	word + ( amount == 1 ? "" : "s" )
+end
+
 
 
 def help_with(keyword)
-	puts case keyword
+	case keyword
 	when "view"
-		puts "This command will output the existing list to the screen"
+		puts "view [all|done|todo]"
+		puts "This command will output the list to the screen as specified"
 	when "add"
 		puts "Anything following this command will be added to the list"
 	when "complete"
 		puts "Anything following this command will removed from the list"
+	when "clean"
+		puts "This command removes all completed entries"
 	when "remove"
 		puts "Anything following this command will removed from the list"
 		puts "Optionally too, entering 'remove all' will clear the list"
+	when "reset"
+		puts "This command erases all local list data. WARNING: cannot be undone!"
 	when "quit", "exit"
 		puts "This command exits the program"
 	else
 		puts "#{keyword} keyword not found."
 	end
+end
+
+define_method :reset do |list|
+	$db.execute("DROP TABLE #{list}");
+	$db.execute(create_table_cmd);
 end
 
 def nl
@@ -105,15 +174,19 @@ end
 
 puts "Hello! Welcome to todolist."
 
-puts "You currently have " + list_count_string(list_name) + " in your list." if list_count(list_name) != 0 
+if list_count(list_name) != 0
+	count = list_count(list_name)
+	puts "You currently have #{count} " + plural("item", count) + " in your list."
+end
 
-puts "commands: view, add *, complete *, remove *, help, quit"
+puts "commands: view, add *, complete *, clean, remove *, help, reset, quit"
 puts "Please enter a command to get started "#('help' for options)"
 
 loop do
 
 	input = ""
 	loop do # input validation loop
+		print "> "
 		input = gets.chomp.strip
 		break if input != ""
 		puts "Please enter valid input"
@@ -125,10 +198,14 @@ loop do
 	nl
 	if input == "view"
 		print_list(list_name)
+	elsif first == "view"
+		print_list(list_name, rest)
 	elsif first == "add"
 		process_addition(rest, list_name)
 	elsif first == "complete"
-		process_removal(rest, list_name)
+		process_completion(rest, list_name)
+	elsif first == "clean"
+		process_clean(list_name)
 	elsif first == "remove"
 		if rest == "all"
 			process_clearance(list_name)
@@ -136,10 +213,18 @@ loop do
 			process_removal(rest, list_name)
 		end
 	elsif input == "help"
-		puts "commands: print, add *, complete *, remove *, help, quit"
+		puts "commands: view, add *, complete *, clean, remove *, help, reset, quit"
 		puts "type 'help' followed by a keyword to find out more!"
 	elsif first == "help"
 		help_with(rest)
+	elsif input == "reset"
+		puts "..."
+		reset(list_name)
+		system "clear" or system "cls"
+		puts "Local list formated. New list created!"
+		puts "commands: view, add *, complete *, clean, remove *, help, reset, quit"
+		puts "Please enter a command to get started "#('help' for options)"
+		redo
 	elsif input == "quit" || input == "exit"
 		puts "Goodbye! Come again."
 		break
